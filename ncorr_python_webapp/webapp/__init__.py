2c6b14992f99c95cf3632f39293e6c0ce9448c8f
Sayyed Ali <104984924+sayyedalimrj@users.noreply.github.com>
2025-05-29 04:20:45 +0330

    Delete ncorr_python_webapp directory

--- ncorr_python_webapp/webapp/__init__.py
+++
@@ -1,5 +0,0 @@
-# This file can be empty or used for package-level initializations.
-# For example, creating and configuring the Flask app instance if not done in app.py directly.
-# from .app import create_app
-#
-# app = create_app() # If you want a global app instance available on import
--- ncorr_python_webapp/webapp/apis/v1/__init__.py
+++
@@ -1,2 +0,0 @@
-# This file makes 'v1' a Python sub-package.
-# It can be empty.
