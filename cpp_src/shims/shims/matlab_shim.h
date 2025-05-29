23e9dbcb3b84e222009b266f8a5064e35ce3f711
Sayyed Ali <104984924+sayyedalimrj@users.noreply.github.com>
2025-05-29 04:28:39 +0330

    Delete cpp_src/shims/shims directory

--- cpp_src/shims/shims/matlab_shim.h
+++
@@ -1,9 +0,0 @@
-#ifndef NCORR_MATLAB_SHIM_H
-#define NCORR_MATLAB_SHIM_H
-
-/*  All legacy sources include **this** header, not mex.h directly.
-    Keep it as a thin wrapper so there is only ONE definition of all
-    the stub symbols.                                                     */
-#include "mex.h"
-
-#endif
